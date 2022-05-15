45e148f9214e181f3f654cacf93d19bca6f70f5b
Hristiyan <h.skenderski@abv.bg>
2022-05-15 16:58:19 +0300

    Project For Exercises

---
+++ StudentInfoSystem/Storage/StudentData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using StudentInfoSystem.Model;
+
+namespace StudentInfoSystem.Storage;
+
+public static class StudentData
+{
+    public static List<Student> TestStudents { get; } = new()
+    {
+        new Student("Hristiyan", "Ivanov", "Skenderski", "FKST", "KSI", "Middle", "Assigned", "121219081", 6, 2, 30)
+    };
+}
